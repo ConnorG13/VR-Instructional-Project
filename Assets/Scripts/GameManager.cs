5c569ea93e970fc907cae74436eb2afe88f2de29
Chase <chasekat742@gmail.com>
2024-04-16 20:48:19 -0500

    set up some methods for task management
    things about to get crazy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,31 @@ public class GameManager : MonoBehaviour
 
 	[SerializeField] private TextCutsceneController text;
 
+	[System.Serializable]
+	private class Task
+	{
+		public string name;
+		public bool complete;
+
+		[Space(10)]
+		[TextArea]
+		public string story;
+
+		[Space(10)]
+		public bool isTimed;
+		public float timeLimit;
+
+		[Space(10)]
+		public bool oneTime;
+	}
+	[SerializeField] private List<Task> tasks;
+
+	private enum TaskState {}
+	private TaskState taskState;
+
+	private Task activeTask;
+	private int taskCount = 0;
+
     void Awake()
     {
 		if (Instance == null) 
@@ -20,5 +45,20 @@ public class GameManager : MonoBehaviour
 	void BeginTasks()
 	{
 		
+	}
+
+	void CompleteTask(string name)
+	{
+		if (name == activeTask.name)
+		{
+			activeTask.complete = true;
+			taskCount++;
+			ChooseTask();
+		}
+	}
+
+	void ChooseTask()
+	{
+
 	}
 }
